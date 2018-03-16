0f3a5a5db9da24e1907b595279e25ade9a397ff1
nswain <nswain@aquaveo.com>
2018-03-16 15:12:17 -0600

    Add ci files and scripts

---
+++ build.py
@@ -0,0 +1,19 @@
+import os
+from conan.packager import ConanMultiPackager
+
+
+if __name__ == "__main__":
+    # ConanPackageTools
+    # See: https://github.com/conan-io/conan-package-tools/blob/develop/README.md
+    builder = ConanMultiPackager()
+    builder.add_common_builds()
+    updated_builds = []
+    # Add environment variables to build definitions
+    xmscore_version = os.environ.get('XMSINTERP_VERSION', None)
+
+    for settings, options, env_vars, build_requires, reference in builder.items:
+        env_vars.update({'XMSINTERP_VERSION': xmscore_version})
+        updated_builds.append([settings, options, env_vars, build_requires])
+    builder.builds = updated_builds
+
+    builder.run()
