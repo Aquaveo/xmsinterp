7648b2f8ada3d725ac071111e41883076cbcdabb
nswain <nswain@aquaveo.com>
2018-05-04 14:53:25 -0600

    Link xmscore before boost in conda builds.

--- condabuildinfo.cmake
+++ condabuildinfo.cmake
@@ -1,9 +1,11 @@
+set(EXT_INCLUDE_DIRS "${CMAKE_INSTALL_PREFIX}/include/xmscore")
+set(EXT_LIB_DIRS "${CMAKE_INSTALL_PREFIX}/lib")
+set(EXT_LIBS xmscore)
+
 set(BOOST_ROOT ${CMAKE_INSTALL_PREFIX})
 set(Boost_USE_STATIC_LIBS TRUE)
-find_package(Boost REQUIRED system filesystem timer serialization log_setup log)
-set(EXT_INCLUDE_DIRS ${Boost_INCLUDE_DIRS})
-set(EXT_LIB_DIRS ${Boost_LIBRARY_DIRS})
-set(EXT_LIBS ${Boost_LIBRARIES})
+find_package(Boost REQUIRED log_setup log system filesystem serialization timer)
+list(APPEND EXT_INCLUDE_DIRS ${Boost_INCLUDE_DIRS})
+list(APPEND EXT_LIB_DIRS ${Boost_LIBRARY_DIRS})
+list(APPEND EXT_LIBS ${Boost_LIBRARIES})
 
-list(APPEND EXT_INCLUDE_DIRS ${CMAKE_INSTALL_PREFIX}/include/xmscore)
-list(APPEND EXT_LIBS xmscore)
