39b6f35430c9dfb55c3af5b7aa8705b0c2ff7577
Chandler Scott <cscott@aquaveo.com>
2018-05-15 12:02:36 -0600

    fix the CI for the python builds

--- condabuildinfo.cmake
+++ condabuildinfo.cmake
@@ -2,7 +2,6 @@ set(EXT_INCLUDE_DIRS "${CMAKE_INSTALL_PREFIX}/include/xmscore")
 set(EXT_LIB_DIRS "${CMAKE_INSTALL_PREFIX}/lib")
 set(EXT_LIBS xmscore)
 
-set(BOOST_ROOT ${CMAKE_INSTALL_PREFIX})
 set(Boost_USE_STATIC_LIBS TRUE)
 find_package(Boost REQUIRED log_setup log system filesystem serialization timer)
 list(APPEND EXT_INCLUDE_DIRS ${Boost_INCLUDE_DIRS})
