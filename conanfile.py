18a3753fbf1be9a8140a31ec6908a790ee4e68b7
Gage Larsen <glarsen@aquaveo.com>
2018-06-29 12:34:37 -0600

    Pybind in xms builds

--- conanfile.py
+++ conanfile.py
@@ -10,8 +10,8 @@ class XmsinterpConan(ConanFile):
     url = "https://github.com/Aquaveo/xmsinterp"
     description = "Interpolation library for XMS products"
     settings = "os", "compiler", "build_type", "arch"
-    options = {"xms": [True, False]}
-    default_options = "xms=False"
+    options = {"xms": [True, False], "pybind": [True, False]}
+    default_options = "xms=False", "pybind=False"
     generators = "cmake"
     build_requires = "cxxtest/4.4@aquaveo/stable"
     exports = "CMakeLists.txt", "LICENSE"
@@ -46,13 +46,18 @@ class XmsinterpConan(ConanFile):
         else:
             self.requires("boost/1.66.0@conan/stable")
             self.requires("xmscore/1.0.25@aquaveo/stable")
-            # self.requires("pybind11/2.2.2@aquaveo/stable")
+        # Pybind if not Visual studio 2013
+        if not (self.settings.compiler == 'Visual Studio' and self.settings.compiler.version < 14) \
+                and self.options.pybind:
+            self.requires("pybind11/2.2.2@aquaveo/stable")
 
     def build(self):
         xms_run_tests = self.env.get('XMS_RUN_TESTS', None)
         run_tests = xms_run_tests != 'None' and xms_run_tests is not None
 
         cmake = CMake(self)
+
+        cmake.definitions["IS_PYTHON_BUILD"] = self.options.pybind
 
         if self.settings.compiler == 'Visual Studio':
             cmake.definitions["XMS_BUILD"] = self.options.xms
