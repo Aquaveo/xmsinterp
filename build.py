43672d70f9478257fb60d9fdabe01ced6c4f8a7f
Gage Larsen <glarsen@aquaveo.com>
2018-07-02 22:52:01 -0600

    fix interp builds

--- build.py
+++ build.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
         if settings['compiler'] == 'Visual Studio' and 'MD' in settings['compiler.runtime']:
             xms_options = dict(options)
             xms_options.update({'xmsinterp:xms': True})
-            if int(settings['compiler.version']) >= 14:
+            if int(settings['compiler.version']) >= 14 and settings['arch'] != "x86":
                 xms_options.update({'xmsinterp:pybind': True})
             updated_builds.append([settings, xms_options, env_vars, build_requires])
 
--- dev/setenv.bat
+++ dev/setenv.bat
@@ -1,10 +1,11 @@
 @echo off
-set XMS_VERSION=99.99.99
+set XMS_VERSION=None
 set XMS_RUN_TESTS=1
+set CONAN_VISUAL_VERSIONS=14
 set CONAN_REFERENCE=xmsinterp/%XMS_VERSION%
 set CONAN_USERNAME=aquaveo
-set CONAN_CHANNEL=stable
+set CONAN_CHANNEL=testing
 set CONAN_GCC_VERSIONS=5
-REM export CONAN_UPLOAD=1
-set | findstr XMS
-set | findstr CONAN
+REM export CONAN_UPLOAD="<conanserver>"
+set | findstr "XMS"
+set | findstr "CONAN"
