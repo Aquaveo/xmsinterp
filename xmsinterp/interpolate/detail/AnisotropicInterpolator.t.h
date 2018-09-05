46378651202dc01e61b32622c773d53e3b2cb609
Bill Dolinar <bdolinar@aquaveo.com>
2018-09-05 17:00:46 -0600

    Added Anisotropic Interpolator.

---
+++ xmsinterp/interpolate/detail/AnisotropicInterpolator.t.h
@@ -0,0 +1,39 @@
+#pragma once
+#ifdef CXX_TEST
+//------------------------------------------------------------------------------
+/// \file
+/// \ingroup interpolate_detail
+/// \copyright (C) Copyright Aquaveo 2018. Distributed under the xmsng
+///  Software License, Version 1.0. (See accompanying file
+///  LICENSE_1_0.txt or copy at http://www.aquaveo.com/xmsng/LICENSE_1_0.txt)
+//------------------------------------------------------------------------------
+
+//----- Included files ---------------------------------------------------------
+// 3. Standard Library Headers
+
+// 4. External Library Headers
+#include <cxxtest/TestSuite.h>
+
+// 5. Shared Headers
+
+// 6. Non-shared Headers
+
+//----- Forward declarations ---------------------------------------------------
+
+//----- Namespace declaration --------------------------------------------------
+// namespace xms {
+
+//----- Constants / Enumerations -----------------------------------------------
+
+//----- Structs / Classes ------------------------------------------------------
+class AnisotropicInterpolatorUnitTests : public CxxTest::TestSuite
+{
+public:
+  void testSimple();
+  void testCrossSectionThroughPoint();
+  void testComplex();
+};
+//----- Function prototypes ----------------------------------------------------
+
+//} // namespace xms
+#endif // CXX_TEST
