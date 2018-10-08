1ed4b7bc29577955a3f55646168dc6977cf65939
Gage Larsen <glarsen@aquaveo.com>
2018-10-08 14:22:52 -0600

    add new test path functionality

--- conanfile.py
+++ conanfile.py
@@ -71,6 +71,7 @@ class XmsinterpConan(ConanFile):
         cmake.definitions["XMS_VERSION"] = '{}'.format(self.version)
         cmake.definitions["IS_PYTHON_BUILD"] = self.options.pybind
         cmake.definitions["BUILD_TESTING"] = self.options.testing
+        cmake.definitions["XMS_TEST_PATH"] = "test_files"
         cmake.definitions["PYTHON_TARGET_VERSION"] = self.env.get("PYTHON_TARGET_VERSION", "3.6")
         cmake.configure(source_folder=".")
         cmake.build()
