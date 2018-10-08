f7ec50e7fef9dcdd023904416f1cd39337fe0550
Gage Larsen <glarsen@aquaveo.com>
2018-10-07 22:40:56 -0600

    update doc generation script

--- generateDocumentationAndDeploy.sh
+++ generateDocumentationAndDeploy.sh
@@ -16,6 +16,7 @@ __AUTHOR__="Aquaveo"
 # - TRAVIS_BUILD_NUMBER : The number of the current build.
 # - TRAVIS_COMMIT       : The commit that the current build is testing.
 # - DOXYFILE            : The Doxygen configuration file.
+# - SPHINX_CONF         : The Sphinx configuration file.
 # - GH_REPO_NAME        : The name of the repository.
 # - GH_REPO_REF         : The GitHub reference to the repository.
 # - GH_REPO_TOKEN       : Secure token to the github repository.
@@ -54,7 +55,7 @@ git config --global push.default simple
 git config user.name "Travis CI"
 git config user.email "travis@travis-ci.org"
 
-# Remove everything currently in the gh-pages branch.
+# Remove everything currently in the gh-pages branch except the python docs.
 # GitHub is smart enough to know which files have changed and which files have
 # stayed the same and will only update the changed files. So the gh-pages branch
 # can be safely cleaned, and it is sure that everything pushed later is the new
@@ -67,52 +68,84 @@ rm -rf *
 # to NO, which it is by default. So creating the file just in case.
 echo "" > .nojekyll
 
+#install doxygen
+sudo apt-get update
+sudo apt-get install -y doxygen doxygen-doc doxygen-latex doxygen-gui graphviz
+
 ################################################################################
 ##### Generate the Doxygen code documentation and log the output.          #####
 echo 'Generating Doxygen code documentation...'
 # Redirect both stderr and stdout to the log file AND the console.
 cd $(dirname $DOXYFILE)
 wget https://raw.githubusercontent.com/Aquaveo/xmscore/gh-pages/xmscore.tag
+doxygen -v
 doxygen $DOXYFILE 2>&1 | tee doxygen.log
 
-# ensure that we do not have doxygen warnings
-if  [ -s 'doxy_warn.log' ]; then cat doxy_warn.log && exit 1; fi;
+# Check for warnings in doxygen
+if [ -s 'doxy_warn.log' ]; then cat doxy_warn.log && exit 1; fi;
+
+# Back out if this is not a tag so we don't post - We can only build python docs
+# on tags because we won't have a python package until it is tagged with a 
+# specific version.
+if [ -z "${TRAVIS_TAG}" ]; then
+  echo "Build not tagged. No Documentation will be uploaded"
+  exit 0
+fi
+
+################################################################################
+##### Generate the Python documentation.                                   #####
+echo 'Generating Python code documentation...'
+# install sphinx and conan
+sudo pip install sphinx sphinx_rtd_theme conan
+# add aquaveo conan remote
+conan user
+conan remote add aquaveo https://conan.aquaveo.com
+# change to directory where the sphinx config file
+cd $(dirname $SPHINX_CONF)
+# make a directory to get the conan package
+mkdir ./conan
+# get the conan package
+conan install -o xmsinterp:pybind=True -s compiler.version=6 -s compiler.libcxx=libstdc++11 -if ./conan -g txt xmsinterp/${TRAVIS_TAG}@aquaveo/stable 
+# get the path to the conan package
+export PATH_TO_PYTHON_PACKAGE=$(cat ./conan/conanbuildinfo.txt | grep PYTHONPATH.*xmsinterpmscore | sed -r 's/^PYTHONPATH=\["(.*?)"\]$/\1/')
+# copy package into build directory
+cp ${PATH_TO_PYTHON_PACKAGE}/* $(dirname $SPHINX_CONF)/
+ls ${PATH_TO_PYTHON_PACKAGE}
+ls $(dirname $SPHINX_CONF)
+# make a directory to hold the python documenation
+mkdir $(dirname $DOXYFILE)/pydocs
+# build the documentation
+sphinx-build -b html . $(dirname $DOXYFILE)/html/pydocs
 
 ################################################################################
 ##### Upload the documentation to the gh-pages branch of the repository.   #####
-# Only upload if Doxygen successfully created the documentation.
-# Check this by verifying that the html directory and the file html/index.html
-# both exist. This is a good indication that Doxygen did it's work.
-#if [ -d "html" ] && [ -f "html/index.html" ]; then
-if [ -d "html" ] && [ -f "html/index.html" ]; then
+# Only upload if Doxygen and Sphinx successfully created the documentation
+# Check this by verifying that the html directory, the file html/index.html,
+# and the html/pydocs/index.html all exist. This is a good indication that 
+# Doxygen and Sphinx did their work.
+cd $(dirname $DOXYFILE)
+if [ -d "html" ] && [ -f "html/index.html" ] && [ -f "html/pydocs/index.html" ]; then
+    mv xmsinterp.tag "$TRAVIS_BUILD_DIR/code_docs/$GH_REPO_NAME/"
+    mv html/* "$TRAVIS_BUILD_DIR/code_docs/$GH_REPO_NAME/"
+    cd $TRAVIS_BUILD_DIR/code_docs/$GH_REPO_NAME
+    echo 'Uploading documentation to the gh-pages branch...'
+    # Add everything in this directory (the Doxygen code documentation) to the
+    # gh-pages branch.
+    # GitHub is smart enough to know which files have changed and which files have
+    # stayed the same and will only update the changed files.
+    git add --all
 
-    # don't upload docs unless we are in "MASTER"
-    if [$TRAVIS_BRANCH -eq "master"]
-    then
-        mv xmsinterp.tag "$TRAVIS_BUILD_DIR/code_docs/$GH_REPO_NAME/"
-        mv html/* "$TRAVIS_BUILD_DIR/code_docs/$GH_REPO_NAME/"
-        cd $TRAVIS_BUILD_DIR/code_docs/$GH_REPO_NAME
-        echo 'Uploading documentation to the gh-pages branch...'
-        # Add everything in this directory (the Doxygen code documentation) to the
-        # gh-pages branch.
-        # GitHub is smart enough to know which files have changed and which files have
-        # stayed the same and will only update the changed files.
-        git add --all
+    # Commit the added files with a title and description containing the Travis CI
+    # build number and the GitHub commit reference that issued this build.
+    git commit -m "Deploy code docs to GitHub Pages Travis build: ${TRAVIS_BUILD_NUMBER}" -m "Commit: ${TRAVIS_COMMIT}"
 
-        # Commit the added files with a title and description containing the Travis CI
-        # build number and the GitHub commit reference that issued this build.
-        git commit -m "Deploy code docs to GitHub Pages Travis build: ${TRAVIS_BUILD_NUMBER}" -m "Commit: ${TRAVIS_COMMIT}"
-
-        # Force push to the remote gh-pages branch.
-        # The ouput is redirected to /dev/null to hide any sensitive credential data
-        # that might otherwise be exposed.
-        git push --force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}" > /dev/null 2>&1
-    else
-        echo "No doxygen warning found. Not on master so documentation is not uploaded."
-    fi
+    # Force push to the remote gh-pages branch.
+    # The ouput is redirected to /dev/null to hide any sensitive credential data
+    # that might otherwise be exposed.
+    git push --force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}" > /dev/null 2>&1
 else
     echo '' >&2
     echo 'Warning: No documentation (html) files have been found!' >&2
     echo 'Warning: Not going to push the documentation to GitHub!' >&2
     exit 1
-fi
+fi 
