1e1099c969b497df7eeee822375a115b8d2afe00
aclark <aclark@aquaveo.com>
2020-11-19 13:33:34 -0700

    Use legacy wchar_t compiler flag for MD builds. Updated boost, xmscore, and xmsgrid requirements. Merge CI fixes from xmscore.
    (cherry picked from commit d8da3e9f796df866989651afe008e43a36e16f44)

--- _package/setup.py
+++ _package/setup.py
@@ -11,8 +11,8 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 requires = [
     'numpy',
-    'xmscore>=4.0.0',
-    'xmsgrid>=5.0.2',
+    'xmscore>=4.0.2',
+    'xmsgrid>=5.0.4',
 ]
 
 version = __version__
