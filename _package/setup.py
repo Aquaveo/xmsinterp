e0dfebe559e5841a4f795d34e4950d36c32f2d7a
aclark <aclark@aquaveo.com>
2020-11-19 16:13:17 -0700

    Update xmsgrid requirement.

--- _package/setup.py
+++ _package/setup.py
@@ -12,7 +12,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 requires = [
     'numpy',
     'xmscore>=4.0.2',
-    'xmsgrid>=5.0.4',
+    'xmsgrid>=5.0.5',
 ]
 
 version = __version__
