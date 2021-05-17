9add5156adbba094e7c7a3767e691c9bb22c9d07
csmemoe-aquaveo <43580760+csmemoe-aquaveo@users.noreply.github.com>
2021-05-17 15:19:29 -0600

    Add and test activity to inGenerateRasterIdw and allow for passing NODATA value into inGenerateRasterIdw.

--- _package/xms/interp/interpolate/interp_util.py
+++ _package/xms/interp/interpolate/interp_util.py
@@ -4,7 +4,7 @@ from .._xmsinterp.interpolate import InterpUtil
 __all__ = ['generate_raster_idw']
 
 
-def generate_raster_idw(pts, min_pt, size, num_rows, num_cols, activity):
+def generate_raster_idw(pts, min_pt, size, num_rows, num_cols, no_data_val, activity):
     """Returns an array of values given mesh information.
     Args:
         pts (iterable): The measured depths at point locations.
@@ -12,9 +12,10 @@ def generate_raster_idw(pts, min_pt, size, num_rows, num_cols, activity):
         size (tuple): Size of the raster cells in the X and Y directions.
         num_rows (int): Number of rows in the raster.
         num_cols (int): Number of cols in the raster.
+        no_data_val (float): The NODATA value to assign to inactive raster pixels in the result.
         activity (iterable): This is the size of the raster return values and is used to determine whether to
          set raster cells to active and interp values to each cell.
     Returns:
         The interpolated values at the given points.
     """
-    return InterpUtil.generate_raster_idw(pts, min_pt, size, num_rows, num_cols, activity)
+    return InterpUtil.generate_raster_idw(pts, min_pt, size, num_rows, num_cols, no_data_val, activity)
