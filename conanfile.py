7346297f8067a2f17e5b752f466e54fc98351440
Alan Lemon <alemon@aquaveo.com>
2021-11-22 13:47:58 -0700

    update conan stuff for CI

--- conanfile.py
+++ conanfile.py
@@ -58,11 +58,10 @@ class XmsinterpConan(ConanFile):
 
         self.requires("xmscore/4.0.2@aquaveo/stable")
         self.requires("xmsgrid/5.0.5@aquaveo/stable")
-
-        if self.settings.os == 'Macos':
-            # Use conan-center-index syntax for Mac
-            # TODO: Use bzip2 package from Aquaveo server when available.
-            self.requires('bzip2/1.0.8')
+        # zlib and bzip2 are required by boost. They used to get pulled automatically from conan-center, but something
+        # changed and we now need to explicitly list them as requirements using the new style notation.
+        self.requires('zlib/1.2.11')
+        self.requires('bzip2/1.0.8')
 
     def build(self):
         cmake = CMake(self)
