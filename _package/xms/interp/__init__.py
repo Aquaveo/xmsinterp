43b56d9ded1a1852a402bf31db7a1838560567c3
Alan Lemon <alemon@aquaveo.com>
2021-12-16 16:43:22 -0700

    -bump python version

--- _package/xms/interp/__init__.py
+++ _package/xms/interp/__init__.py
@@ -3,4 +3,4 @@ from . import interpolate  # NOQA: F401
 from .api.interpolator import interpolate_to_grid  # NOQA: F401
 from .api.interpolator import interpolate_to_points  # NOQA: F401
 
-__version__ = '4.1.0'
+__version__ = '4.1.3'
