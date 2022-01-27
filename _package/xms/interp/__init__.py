4ca546428336a01c905a4173429f2a2fef53020d
Thomas Christiansen <tchristiansen@aquaveo.local>
2022-01-27 14:15:33 -0700

    Update version string.

--- _package/xms/interp/__init__.py
+++ _package/xms/interp/__init__.py
@@ -3,4 +3,4 @@ from . import interpolate  # NOQA: F401
 from .api.interpolator import interpolate_to_grid  # NOQA: F401
 from .api.interpolator import interpolate_to_points  # NOQA: F401
 
-__version__ = '4.1.4'
+__version__ = '5.0.0'
