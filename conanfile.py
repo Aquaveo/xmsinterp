6ea52b34bf68827339237431fe81949701edb9ad
Gage Larsen <gagelarsen53@gmail.com>
2022-03-10 14:21:49 -0700

    fix package name.

--- conanfile.py
+++ conanfile.py
@@ -4,16 +4,16 @@ conanfile.py for the xmsinterp project.
 from xmsconan.xms_conan_file import XmsConanFile
 
 
-class XmscoreConanFile(XmsConanFile):
+class XmsinterpConanFile(XmsConanFile):
     """
-    XmscoreConan class used for defining the conan info.
+    XmsinterpConan class used for defining the conan info.
     """
-    name = 'xmscore'
-    url = 'https://github.com/Aquaveo/xmscore'
-    description = 'Support library for XMS products'
+    name = 'xmsinterp'
+    url = 'https://github.com/Aquaveo/xmsinterp'
+    description = 'Interpolation library for XMS products'
     xms_dependencies = [
         "xmscore/[>=6.0.1 <7.0.0]@aquaveo/stable",
         "xmsgrid/[>=7.0.1 <8.0.0]@aquaveo/stable",
     ]
 
-LIBRARY_NAME = XmscoreConanFile.name
+LIBRARY_NAME = XmsinterpConanFile.name
