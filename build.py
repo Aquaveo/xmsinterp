e1d093bafc77d52d9af1fac8eeeccc43a59fdc00
Gage Larsen <gagelarsen53@gmail.com>
2022-03-10 13:05:19 -0700

    changes to unify shared code

--- build.py
+++ build.py
@@ -1,84 +1,12 @@
-import os
-from cpt.packager import ConanMultiPackager
+
+"""
+The build.py file for the xms project.
+"""
+from xmsconan.build_helpers import get_builder
+
+from conanfile import LIBRARY_NAME
 
 
 if __name__ == "__main__":
-    # ConanPackageTools
-    # See: https://github.com/conan-io/conan-package-tools/blob/develop/README.md
-    builder = ConanMultiPackager()
-    builder.add_common_builds()
-
-    # Add environment variables to build definitions
-    XMS_VERSION = os.environ.get('XMS_VERSION', None)
-    python_target_version = os.environ.get('PYTHON_TARGET_VERSION', "3.10")
-    release_python = os.environ.get('RELEASE_PYTHON', 'False')
-    aquapi_username = os.environ.get('AQUAPI_USERNAME', None)
-    aquapi_password = os.environ.get('AQUAPI_PASSWORD', None)
-    aquapi_url = os.environ.get('AQUAPI_URL', None)
-
-    for settings, options, env_vars, build_requires, reference in builder.items:
-        # General Options
-        env_vars.update({
-            'XMS_VERSION': XMS_VERSION,
-            'VERBOSE': 1,
-            'PYTHON_TARGET_VERSION': python_target_version,
-            'RELEASE_PYTHON': release_python,
-            'AQUAPI_USERNAME': aquapi_username,
-            'AQUAPI_PASSWORD': aquapi_password,
-            'AQUAPI_URL': aquapi_url,
-        })
-
-        # Require c++11 compatibility
-        if settings['compiler'] == 'gcc':
-            settings.update({
-                'compiler.libcxx': 'libstdc++11'
-            })
-            compiler_version = int(settings['compiler.version'])
-            if compiler_version in [5, 6]:
-                settings.update({'cppstd': '14'})
-            elif compiler_version == 7:
-                settings.update({'cppstd': '17'})
-        elif settings['compiler'] == 'apple-clang':
-            settings.update({'cppstd': 'gnu17'})
-        elif settings['compiler'] == 'Visual Studio':
-            settings.update({'cppstd': '17'})
-
-    pybind_updated_builds = []
-    for settings, options, env_vars, build_requires, reference in builder.items:
-        # pybind option
-        if (not settings['compiler'] == "Visual Studio" \
-                     or int(settings['compiler.version']) > 12) \
-                and settings['arch'] == "x86_64" and settings['build_type'] != 'Debug':
-            pybind_options = dict(options)
-            pybind_options.update({'xmsinterp:pybind': True})
-            pybind_updated_builds.append([settings, pybind_options, env_vars, build_requires])
-
-        pybind_updated_builds.append([settings, options, env_vars, build_requires])
-    builder.builds = pybind_updated_builds
-
-    testing_updated_builds = []
-    for settings, options, env_vars, build_requires, reference in builder.items:
-        # xms option
-        if not options.get('xmsinterp:xms', False) and not options.get('xmsinterp:pybind', False):
-            testing_options = dict(options)
-            testing_options.update({'xmsinterp:testing': True})
-            testing_updated_builds.append([settings, testing_options, env_vars, build_requires])
-        testing_updated_builds.append([settings, options, env_vars, build_requires])
-    builder.builds = testing_updated_builds
-
-    wchar_updated_builds = []
-    for settings, options, env_vars, build_requires, reference in builder.items:
-        # wchar_t option
-        if settings['compiler'] == 'Visual Studio' and not options.get('xmsinterp:pybind', False):
-            wchar_options = dict(options)
-            wchar_options.update({'xmsinterp:wchar_t': 'typedef'})
-            wchar_updated_builds.append([settings, wchar_options, env_vars, build_requires])
-        elif settings['compiler'] == 'Visual Studio':
-            wchar_options = dict(options)
-            wchar_options.update({'xmsinterp:wchar_t': 'builtin'})
-            wchar_updated_builds.append([settings, wchar_options, env_vars, build_requires])
-        else:
-            wchar_updated_builds.append([settings, options, env_vars, build_requires])
-    builder.builds = wchar_updated_builds
-
+    builder = get_builder(LIBRARY_NAME)
     builder.run()
--- conanfile.py
+++ conanfile.py
@@ -1,140 +1,19 @@
-import os
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanException
+"""
+conanfile.py for the xmsinterp project.
+"""
+from xmsconan.xms_conan_file import XmsConanFile
 
 
-class XmsinterpConan(ConanFile):
-    name = "xmsinterp"
-    # version = None  # This no longer worked after conan version 1.11
-    license = "XMSNG Software License"
-    url = "https://github.com/Aquaveo/xmsinterp"
-    description = "Interpolation library for XMS products"
-    settings = "os", "compiler", "build_type", "arch"
-    options = {
-        "wchar_t": ['builtin', 'typedef'],
-        "pybind": [True, False],
-        "testing": [True, False],
-    }
-    default_options = {
-        'wchar_t': 'builtin',
-        'pybind': False,
-        'testing': False,
-    }
-    generators = "cmake"
-    build_requires = "cxxtest/4.4@aquaveo/stable"
-    exports = "CMakeLists.txt", "LICENSE"
-    exports_sources = "xmsinterp/*", "_package/*"
+class XmscoreConanFile(XmsConanFile):
+    """
+    XmscoreConan class used for defining the conan info.
+    """
+    name = 'xmscore'
+    url = 'https://github.com/Aquaveo/xmscore'
+    description = 'Support library for XMS products'
+    xms_dependencies = [
+        "xmscore/[>=6.0.1 <7.0.0]@aquaveo/stable",
+        "xmsgrid/[>=7.0.1 <8.0.0]@aquaveo/stable",
+    ]
 
-    def configure(self):
-        # Set verion dynamically using XMS_VERSION env variable.
-        self.version = self.env.get('XMS_VERSION', '99.99.99')
-
-        # Raise ConanExceptions for Unsupported Versions
-        s_os = self.settings.os
-        s_compiler = self.settings.compiler
-        s_compiler_version = self.settings.compiler.version
-
-        if s_compiler == "apple-clang" and s_os == 'Linux':
-            raise ConanException("Clang on Linux is not supported.")
-
-        if s_compiler == "apple-clang" \
-                and s_os == 'Macos' \
-                and float(s_compiler_version.value) < 9.0:
-            raise ConanException("Clang > 9.0 is required for Mac.")
-
-        if s_compiler == 'Visual Studio' and self.options.wchar_t == 'typedef' and self.options.pybind:
-            raise ConanException("wchar_t=typedef not supported with pybind=True")
-
-        self.options['xmscore'].pybind = self.options.pybind
-        self.options['xmscore'].testing = self.options.testing
-
-        self.options['xmsgrid'].pybind = self.options.pybind
-        self.options['xmsgrid'].testing = self.options.testing
-
-        if s_compiler == 'Visual Studio':
-            self.options['xmscore'].wchar_t = self.options.wchar_t
-            self.options['xmsgrid'].wchar_t = self.options.wchar_t
-            self.options['boost'].wchar_t = self.options.wchar_t
-
-    def config_options(self):
-        if self.settings.compiler != 'Visual Studio':
-            del self.options.wchar_t
-
-    def requirements(self):
-        """Requirements."""
-        self.requires("boost/1.74.0.3@aquaveo/stable")
-
-        if self.options.pybind:
-            self.requires("pybind11/2.9.1@aquaveo/stable")
-
-        self.requires("xmscore/6.0.0@aquaveo/stable")
-        self.requires("xmsgrid/7.0.0@aquaveo/stable")
-
-    def build(self):
-        cmake = CMake(self)
-
-        # CXXTest doesn't play nice with PyBind. Also, it would be nice to not
-        # have tests in release code. Thus, if we want to run tests, we will
-        # build a test version (without python), run the tests, and then (on
-        # sucess) rebuild the library without tests.
-        cmake.definitions["XMS_VERSION"] = '{}'.format(self.version)
-        cmake.definitions["IS_PYTHON_BUILD"] = self.options.pybind
-        cmake.definitions["BUILD_TESTING"] = self.options.testing
-        cmake.definitions["XMS_TEST_PATH"] = "test_files"
-        cmake.definitions["PYTHON_TARGET_VERSION"] = self.env.get("PYTHON_TARGET_VERSION", "3.10")
-        if self.settings.compiler == 'Visual Studio':
-            cmake.definitions["USE_NATIVE_WCHAR_T"] = (self.options.wchar_t == 'builtin')
-        cmake.configure(source_folder=".")
-        cmake.build()
-        cmake.install()
-
-        if self.options.testing:
-            print("***********(0.0)*************")
-            try:
-                cmake.test()
-            except ConanException:
-                raise
-            finally:
-                if os.path.isfile("TEST-cxxtest.xml"):
-                    with open("TEST-cxxtest.xml", "r") as f:
-                        for line in f.readlines():
-                            no_newline = line.strip('\n')
-                            print(no_newline)
-                print("***********(0.0)*************")
-        elif self.options.pybind:
-            with tools.pythonpath(self):
-                if not self.settings.os == "Macos":
-                  self.run('pip install --user numpy wheel')
-                else:
-                  self.run('pip install numpy wheel')
-                self.run('python -m unittest discover -v -p *_pyt.py -s {}/_package/tests'.format(
-                    os.path.join(self.build_folder)), cwd=os.path.join(self.package_folder, "_package"))
-                # Create and upload wheel to aquapi if release and windows
-                # We are uploading to aquapi here instead of pypi because pypi doesn't accept
-                # the type of package 'linux_x86_64 that we want to upload. They only accept
-                # manylinux1 as the plat-tag
-                is_release = self.env.get("RELEASE_PYTHON", 'False') == 'True'
-                is_mac_os = self.settings.os == 'Macos'
-                is_gcc_6 = self.settings.os == "Linux" and float(self.settings.compiler.version.value) == 6.0
-                is_windows_md = (self.settings.os == "Windows" and str(self.settings.compiler.runtime) == "MD")
-                if is_release and (is_mac_os or is_gcc_6 or is_windows_md):
-                    devpi_url = self.env.get("AQUAPI_URL", 'NO_URL')
-                    devpi_username = self.env.get("AQUAPI_USERNAME", 'NO_USERNAME')
-                    devpi_password = self.env.get("AQUAPI_PASSWORD", 'NO_PASSWORD')
-                    self.run('devpi use {}'.format(devpi_url))
-                    self.run('devpi login {} --password {}'.format(devpi_username, devpi_password))
-                    plat_names = {'Windows': 'win_amd64', 'Linux': 'linux_x86_64', "Macos": 'macosx-10.6-intel'}
-                    self.run('python setup.py bdist_wheel --plat-name={} --dist-dir {}'.format(
-                        plat_names[str(self.settings.os)],
-                        os.path.join(self.build_folder, "dist")), cwd=os.path.join(self.package_folder, "_package"))
-                    self.run('devpi upload --from-dir {}'.format(os.path.join(self.build_folder, "dist")), cwd=".")
-
-    def package(self):
-        self.copy("license", dst="licenses", ignore_case=True, keep_path=False)
-
-    def package_info(self):
-        self.env_info.PYTHONPATH.append(os.path.join(self.package_folder, "_package"))
-        if self.settings.build_type == 'Debug':
-            self.cpp_info.libs = ["xmsinterplib_d"]
-        else:
-            self.cpp_info.libs = ["xmsinterplib"]
+LIBRARY_NAME = XmscoreConanFile.name
