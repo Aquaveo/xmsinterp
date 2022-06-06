cc996eec33f3eb00411f35f17918738cc2cd3c25
Gage Larsen <gagelarsen53@gmail.com>
2022-06-06 10:12:47 -0600

    Add InterpLinearExtrapIdw

--- _package/xms/interp/interpolate/__init__.py
+++ _package/xms/interp/interpolate/__init__.py
@@ -3,4 +3,5 @@ from . import interp_util  # NOQA: F401
 from .interp_anisotropic import InterpAnisotropic  # NOQA: F401
 from .interp_idw import InterpIdw  # NOQA: F401
 from .interp_linear import InterpLinear  # NOQA: F401
+from .interp_linear_extrap_idw import InterpLinearExtrapIdw  # NOQA: F401
 from .interpolator import Interpolator  # NOQA: F401
