86966eb0f93fbdcfe0be67ff33c27b3efeb98544
Gage Larsen <glarsen@aquaveo.com>
2025-01-22 15:53:33 -0700

    update to conan2

--- build.py
+++ build.py
@@ -2,11 +2,24 @@
 """
 The build.py file for the xms project.
 """
-from xmsconan.build_helpers import get_builder
+import argparse
 
-from conanfile import LIBRARY_NAME
+from xmsconan.package_tools import packager
+
+import conanfile
+
+# arg parse for skip build and skip upload
+parser = argparse.ArgumentParser()
+parser.add_argument("--skip-build", help="Skip the build step", action="store_true")
+parser.add_argument("--skip-upload", help="Skip the upload step", action="store_true")
+parser.add_argument("--version", help="Version to upload", default='*')
 
 
 if __name__ == "__main__":
-    builder = get_builder(LIBRARY_NAME)
-    builder.run()
+    args = parser.parse_args()
+    builder = packager.XmsConanPackager(conanfile.LIBRARY_NAME, conanfile.__file__)
+    builder.generate_configurations()
+    if not args.skip_build:
+        builder.run()
+    if not args.skip_upload:
+        builder.upload(version=args.version)
