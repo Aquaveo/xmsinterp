484b66f83e40bced4a8e5c6ec7eb4fce1404a1e8
Bill Dolinar <bill_dolinar@yahoo.com>
2025-01-28 14:09:17 -0700

    Use template for pyproject.toml file.

--- _package/xms/interp/__init__.py
+++ _package/xms/interp/__init__.py
@@ -3,4 +3,3 @@ from . import interpolate  # NOQA: F401
 from .api.interpolator import interpolate_to_grid  # NOQA: F401
 from .api.interpolator import interpolate_to_points  # NOQA: F401
 
-__version__ = '6.1.7'
